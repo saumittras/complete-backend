55b8c7d913ed4692bffe365c6662a911e9cf74a2
saumittras <saumittra.eee@gmail.com>
2025-05-28 23:03:57 +0600

    added moduler system

--- 2. node-module-system/index.js
+++ 2. node-module-system/index.js
@@ -1,7 +1,7 @@
 // 1. module.exports -> export
 // 2. require  -> import
 
-const firstModule = require('./first-module'); 
+const firstModule = require ('./first-module'); 
 //  import firstModule from './first-module'
 
 console.log(firstModule.add(10,20))
